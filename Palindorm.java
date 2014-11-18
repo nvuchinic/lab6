aa930b21383e89ea01e1336ab19bb1f768262996
nerav <nermin.vucinic@gmail.com>
2014-11-18 09:37:16 +0100

    Add Palindorm.java

---
+++ Palindorm.java
@@ -0,0 +1,9 @@
+package lab6;
+
+public class Palindorm {
+public static void main(String[] args) {
+	String s="abcde";
+	
+	System.out.print(s+s.charAt(3)+s.charAt(2)+s.charAt(1)+s.charAt(0));
+}
+}
