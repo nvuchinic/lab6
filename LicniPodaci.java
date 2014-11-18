ebb316a5660efb861046d59ec68b2018b902efed
nerav <nermin.vucinic@gmail.com>
2014-11-18 09:35:01 +0100

    Add LicniPodaci.java

---
+++ LicniPodaci.java
@@ -0,0 +1,11 @@
+package lab6;
+
+public class LicniPodaci {
+public static void main(String[] args) {
+	String mojeIme="Nermin";
+	String mojePrezime="Vučinic";
+	String datumRod="02.09.1981";
+	String spol="muski";
+	System.out.println("Ime: "+mojeIme +"\nPrezime: "+mojePrezime+"\nDatum rodjenja rodjenja: "+datumRod+"\n Spol: "+spol );
+}
+}
