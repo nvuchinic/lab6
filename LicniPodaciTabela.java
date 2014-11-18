947f57e69d6cb0bf6bc679d6a0d8aea1979dcaf9
nerav <nermin.vucinic@gmail.com>
2014-11-18 09:36:23 +0100

    Add LicniPodaciTabela

---
+++ LicniPodaciTabela.java
@@ -0,0 +1,11 @@
+package lab6;
+
+public class LicniPodaciTabela {
+public static void main(String[] args) {
+	String ime="Nermin";
+	String prezime="Vucinic";
+	String datumRod="02.09.1981.";
+	System.out.print("PODACI"+"\n\n\nIME: \t\t\t"+ime +"\nPREZIME: \t\t"+prezime+"\nDATUM RODJENJA: \t"+datumRod);
+
+}
+}
